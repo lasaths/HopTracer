97cc5fd8b49b321018e2ece368200bd8753914a7
Lasath Siriwardena <lasath.siriwardena@icd.uni-stuttgart.de>
2025-11-24 18:38:52 +0100

    Update UI logos in index.html and diff_viewer.html

--- src_csharp/HopTracer.Web/Program.cs
+++ src_csharp/HopTracer.Web/Program.cs
@@ -1,7 +1,13 @@
+using HopTracer.Core.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 builder.Services.AddControllers();
+builder.Services.AddSingleton<IGhxParser, GhxParser>();
+builder.Services.AddSingleton<IDiffer, Differ>();
+builder.Services.AddSingleton<IConverterService, ConverterService>();
+builder.Services.AddSingleton<IGitWrapper, GitWrapper>();
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
